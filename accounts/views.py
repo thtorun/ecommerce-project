3ef41aa2aa36259f257b052104af4312e68a81c7
thtorun <64398517+thtorun@users.noreply.github.com>
2020-05-14 17:51:35 +0600

    1st commit

---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                messages.info(request, 'Successfully Done')
+
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
